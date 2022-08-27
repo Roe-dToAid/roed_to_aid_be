3717eea4dde2067cb888fc39654c4487c016e760
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-08-27 15:54:44 -0600

    Add routes for authorized clinics

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/resources', to: 'resources#index'
       get '/misinformation_centers', to: 'misinformation_centers#index'
+      get '/authorized_clinics', to: 'authorized_clinics#index'
       resources :states, only: %i[index show] do
         get '/resources', to: 'resources#show'
         get '/misinformation_centers', to: 'misinformation_centers#show'
+        get '/authorized_clinics', to: 'authorized_clinics#show'
       end
     end
   end
--- spec/requests/api/v1/authorized_clinic_spec.rb
+++ spec/requests/api/v1/authorized_clinic_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe 'Authentic Clinic Endpoint', type: :request do 
+RSpec.describe 'Authorized Clinic Endpoint', type: :request do 
   describe 'happy path' do 
     describe 'index endpoint' do 
-      it 'returns all authentic clinics' do 
+      it 'returns all authorized clinics' do 
         state = State.create!(name: 'Texas', abbreviation: 'TX', legal: 2, legal_description: 'banned as heck', source: 'website')
         clinic1 = AuthorizedClinic.create!(state_id: state.id, name: 'Planned Parenthood', address: 'its sad because there actually arent any clinics in TX', city: 'Houston', zip: 87108, phone: '5555555555', services: 'pill, clinic', website: 'a super cool webpage')
+        clinic2 = AuthorizedClinic.create!(state_id: state.id, name: 'Fake Clinic', address: '123 Seaside Ln', city: 'Atlantis', zip: 87345, phone: '4556439900', services: 'clinic', website: 'another super cool webpage')
 
         get '/api/v1/authorized_clinics'
 
