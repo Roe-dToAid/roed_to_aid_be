7ad9110905f1dee636c7255e65efbb2cd8c2a3de
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-08-27 16:18:53 -0600

    Add show for clinics by state

--- spec/requests/api/v1/authorized_clinic_spec.rb
+++ spec/requests/api/v1/authorized_clinic_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'Authorized Clinic Endpoint', type: :request do
         state2 = State.create!(name: 'New Mexico', abbreviation: 'NM', legal: 0, legal_description: 'fully legal', source: 'website')
 
         clinic1 = AuthorizedClinic.create!(state_id: state.id, name: 'Planned Parenthood', address: 'its sad because there actually arent any clinics in TX', city: 'Houston', zip: 87108, phone: '5555555555', services: 'pill, clinic', website: 'a super cool webpage')
-        clinic2 = AuthorizedClinic.create!(state_id: state.id, name: 'Fake Clinic', address: '123 Seaside Ln', city: 'Atlantis', zip: 87345, phone: '4556439900', services: 'clinic', website: 'another super cool webpage')
+        clinic2 = AuthorizedClinic.create!(state_id: state2.id, name: 'Fake Clinic', address: '123 Seaside Ln', city: 'Atlantis', zip: 87345, phone: '4556439900', services: 'clinic', website: 'another super cool webpage')
 
         get '/api/v1/authorized_clinics'
 
@@ -22,6 +22,25 @@ RSpec.describe 'Authorized Clinic Endpoint', type: :request do
         expect(clinics[:data].first[:attributes]).to have_key(:name)
         expect(clinics[:data].first[:attributes]).to have_key(:address)
         expect(clinics[:data].first[:attributes]).to have_key(:services)
+      end
+    end 
+
+    describe 'show endpoint' do 
+      it 'returns successful but empty with no clinics' do 
+        state = State.create!(name: 'Texas', abbreviation: 'TX', legal: 2, legal_description: 'banned as heck', source: 'website')
+        state2 = State.create!(name: 'New Mexico', abbreviation: 'NM', legal: 0, legal_description: 'fully legal', source: 'website')
+
+        clinic1 = AuthorizedClinic.create!(state_id: state2.id, name: 'Planned Parenthood', address: 'its sad because there actually arent any clinics in TX', city: 'Houston', zip: 87108, phone: '5555555555', services: 'pill, clinic', website: 'a super cool webpage')
+        clinic2 = AuthorizedClinic.create!(state_id: state2.id, name: 'Fake Clinic', address: '123 Seaside Ln', city: 'Atlantis', zip: 87345, phone: '4556439900', services: 'clinic', website: 'another super cool webpage')
+
+        get '/api/v1/states/TX/authorized_clinics'
+
+        expect(response).to be_successful
+
+        clinics = JSON.parse(response.body, symbolize_names: true)
+        # binding.pry
+        expect(clinics).to be_a(Hash)
+        expect(clinics[:data].count).to eq(0)
       end
     end
   end
