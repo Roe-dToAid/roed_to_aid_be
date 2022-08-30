f5c54f9764e2b1059db4381b2a6b2e43a4034453
Bryce Wein <bryce.wein@gmail.com>
2022-08-30 12:34:11 -0400

    Fix: make all accessable cors
    Co-authored-by: Casey Fazio <98674727+casefaz@users.noreply.github.com>
    Co-authored-by: Wesley Garcia <98676136+wesatt@users.noreply.github.com>

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://roed-to-aide-fe.herokuapp.com/'
+    # origins 'http://localhost:3000', 'https://roed-to-aide-fe.herokuapp.com/'
+    origins '*'
 
     resource '*',
-      headers: :any,
-      # methods: [:get, :post, :put, :patch, :delete, :options, :head]
-      methods: [:get]
+             headers: :any,
+             # methods: [:get, :post, :put, :patch, :delete, :options, :head]
+             methods: [:get]
   end
 end
