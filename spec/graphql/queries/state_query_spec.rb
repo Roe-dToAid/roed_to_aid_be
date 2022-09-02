9eb2279fbbefe006b8324daa2ce35498bafc26e3
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-09-02 14:32:20 -0600

    add test for GQL return with single state
    Co-authored-by: Wesley Garcia <98676136+wesatt@users.noreply.github.com>
    
    Co-authored-by: Bryce Wein <85247765+bwbolt@users.noreply.github.com>

--- spec/graphql/queries/state_query_spec.rb
+++ spec/graphql/queries/state_query_spec.rb
@@ -10,12 +10,10 @@ RSpec.describe 'GraphQL State Queries', type: :request do
           source: 'website_here_NM')
 
         user = User.create!(email: 'test user', token: '1b86bdaac6dde78337da1a8618f71bfd')
-        body = { api_key: '1b86bdaac6dde78337da1a8618f71bfd' }
 
         post '/graphql?api_key=1b86bdaac6dde78337da1a8618f71bfd', params: { query: states }
 
         expect(response).to be_successful
-        # binding.pry
         json = JSON.parse(response.body)
         states = json['data']['states']
     
@@ -27,16 +25,53 @@ RSpec.describe 'GraphQL State Queries', type: :request do
           'legalDescription' => be_present,
           'source' => be_present,
         )
-
-        # binding.pry
       end
     end
+  end
+
+  describe 'state' do 
+    describe 'happy paths' do
+      it 'successfully queries the API' do 
+        state1 = State.create!(name: 'Texas', abbreviation: 'TX', legal: 2, legal_description: 'lorem ipsum_TX',
+        source: 'website_here_TX')
+        user = User.create!(email: 'test user', token: '1b86bdaac6dde78337da1a8618f71bfd')
+
+        post '/graphql?api_key=1b86bdaac6dde78337da1a8618f71bfd', params: { query: state(abbreviation: state1.abbreviation) }
+
+        expect(response).to be_successful
+        json = JSON.parse(response.body)
+        states = json['data']['state']
+    
+        expect(json).to be_a(Hash)
+        expect(states).to include(
+          'name' => be_present,
+          'abbreviation' => be_present,
+          'legal' => be_present,
+          'legalDescription' => be_present,
+          'source' => be_present,
+        )
+      end
+    end 
   end
 
   def states
     <<~GQL
       query {
         states {
+          name
+          abbreviation
+          legal
+          legalDescription
+          source
+        }
+      }
+    GQL
+  end
+
+  def state(abbreviation:)
+    <<~GQL
+      query {
+        state(abbreviation: "#{abbreviation}") {
           name
           abbreviation
           legal
