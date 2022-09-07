63cf42d4ce68cdc575ba5ea2170a04f5becc714f
Wesley Garcia <wes@alltradestech.com>
2022-09-07 14:23:16 -0700

    Update link on index page
    Co-authored-by: Casey Fazio <98674727+casefaz@users.noreply.github.com>
    Co-authored-by: Bryce Wein <85247765+bwbolt@users.noreply.github.com>

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,6 @@ class WelcomeController < ApplicationController
   skip_before_action :api_key_verification
 
   def index
-    render html: 'Please Visit <a href="https://roed-to-aide-fe.herokuapp.com/">Roe\'d To Aid</a>'.html_safe
+    render html: 'Please Visit <a href="https://roed-to-aid.herokuapp.com/">Roe\'d To Aid</a>'.html_safe
   end
 end
